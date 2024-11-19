89ab2f1558d1e12c4d1eacfadd334b47f933c590
Grian Gajila <griangajila2@gmail.com>
2024-11-19 16:17:11 +0800

    chore: setup tailwind css using nativewind

---
+++ app/index.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+
+export default function HomeScreen() {
+	return (
+		<View className="bg-purple-500 flex justify-center items-center h-screen">
+			<Text className="text-blue-50">Hello</Text>
+			<Text className="text-blue-50">World</Text>
+		</View>
+	);
+}
